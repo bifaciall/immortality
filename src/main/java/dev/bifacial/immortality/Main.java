0cf80511831b7b8966f773e829e448e7e15f61de
allerliebst <davidjessen89@gmail.com>
2022-11-01 17:21:40 +0100

    Initial Commit

--- src/main/java/dev/bifacial/immortality/Main.java
+++ src/main/java/dev/bifacial/immortality/Main.java
@@ -6,7 +6,7 @@ public final class Main extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        System.out.println("Started Immortality!");
+        System.out.println("Started Immortality");
 
     }
 
