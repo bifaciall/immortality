838b0b96855fdaff3329cd15727569075ea29058
allerliebst <davidjessen89@gmail.com>
2022-11-01 18:15:56 +0100

    added EventListener and basic functionality

--- src/main/java/dev/bifacial/immortality/Main.java
+++ src/main/java/dev/bifacial/immortality/Main.java
@@ -1,5 +1,8 @@
 package dev.bifacial.immortality;
 
+import org.bukkit.Bukkit;
+import org.bukkit.event.Event;
+import org.bukkit.event.Listener;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Main extends JavaPlugin {
@@ -7,6 +10,7 @@ public final class Main extends JavaPlugin {
     @Override
     public void onEnable() {
         System.out.println("Started Immortality");
+
 
     }
 
