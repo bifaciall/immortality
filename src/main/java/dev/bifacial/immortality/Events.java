c26090ec4176dd89137199ce0247a0fc63ea6a84
allerliebst <davidjessen89@gmail.com>
2022-11-01 18:37:47 +0100

    removed String from the ArrayList Object

--- src/main/java/dev/bifacial/immortality/Events.java
+++ src/main/java/dev/bifacial/immortality/Events.java
@@ -8,22 +8,23 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 import java.util.ArrayList;
+import java.util.Objects;
 
 
 public class Events extends JavaPlugin implements Listener {
-    
+
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent deathEvent){
         ItemStack skull = new ItemStack(Material.PLAYER_HEAD);
         SkullMeta skullMeta = (SkullMeta) skull.getItemMeta();
         skullMeta.displayName(deathEvent.getPlayer().displayName());
-        ArrayList<String> lore = new ArrayList<String>();
+        ArrayList<String> lore = new ArrayList<>();
         lore.add("Du besitzt nun den Kopf von " + deathEvent.getPlayer().getName() + ". Du kannst ihn benutzen um diesen oder einen anderen Spieler wiederzubeleben");
         skull.setLore(lore);
         skullMeta.setOwner(deathEvent.getPlayer().getName());
         skullMeta.setUnbreakable(true);
         skull.setItemMeta(skullMeta);
-        deathEvent.getPlayer().getKiller().getInventory().addItem(skull);
+        Objects.requireNonNull(deathEvent.getPlayer().getKiller()).getInventory().addItem(skull);
 
     }
 
