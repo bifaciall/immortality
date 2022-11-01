3e343f706bfdaa728fb2401cc68fc933aa578ca4
allerliebst <davidjessen89@gmail.com>
2022-11-01 17:12:57 +0100

    Initial Commit

---
+++ src/main/java/dev/bifacial/immortality/Main.java
@@ -0,0 +1,17 @@
+package dev.bifacial.immortality;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Main extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        System.out.println("Started Immortality");
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
