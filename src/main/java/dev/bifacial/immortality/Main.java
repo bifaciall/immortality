e652cf0233416fff09a54891825cf81f36d70879
allerliebst <davidjessen89@gmail.com>
2023-05-04 22:02:55 +0200

    Kommentar weg

--- src/main/java/dev/bifacial/immortality/Main.java
+++ src/main/java/dev/bifacial/immortality/Main.java
@@ -4,22 +4,9 @@ import dev.bifacial.immortality.Listeners.DeathEvent;
 import dev.bifacial.immortality.Listeners.HeadPlaceEvent;
 import dev.bifacial.immortality.Listeners.RightClickEvent;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
-import org.bukkit.GameMode;
-import org.bukkit.Material;
-import org.bukkit.entity.Player;
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.block.BlockPlaceEvent;
-import org.bukkit.event.entity.PlayerDeathEvent;
-import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.meta.SkullMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.Collection;
-import java.util.Objects;
 
 public final class Main extends JavaPlugin implements Listener{
 
