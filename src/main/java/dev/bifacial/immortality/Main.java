8290f3debdc80c7780b07b12371f599feea045a9
allerliebst <davidjessen89@gmail.com>
2023-05-04 22:02:27 +0200

    Kommentar weg

--- src/main/java/dev/bifacial/immortality/Main.java
+++ src/main/java/dev/bifacial/immortality/Main.java
@@ -37,21 +37,6 @@ public final class Main extends JavaPlugin implements Listener{
     public void onDisable() {
         getServer().getLogger().info("Disabled.");
     }
-//    @EventHandler
-//    public void onPlace(BlockPlaceEvent blockPlaceEvent) {
-//        boolean on = false;
-//        Player p = Bukkit.getPlayer(blockPlaceEvent.getItemInHand().getItemMeta().getDisplayName());
-//        if (blockPlaceEvent.getBlock().getType().equals(Material.PLAYER_HEAD) && !blockPlaceEvent.getItemInHand().getItemMeta().getDisplayName().equals("Player Head")) {
-//            assert p != null;
-//            if (p.isOnline()) p.setGameMode(GameMode.SURVIVAL); on = true;
-//            p.teleport(blockPlaceEvent.getBlock().getLocation());
-//        }
-//        if (blockPlaceEvent.getBlock().getType().equals(Material.PLAYER_HEAD)) {
-//            blockPlaceEvent.setCancelled(true);
-//            if (on){ blockPlaceEvent.getPlayer().getInventory().remove(blockPlaceEvent.getItemInHand()); assert p != null; blockPlaceEvent.getPlayer().sendMessage(ChatColor.GREEN + "Der Spieler " + p.getName() + " wurde wiederbelebt");}
-//            else blockPlaceEvent.getPlayer().sendMessage("Dieser Spieler ist momentan nicht Online.");
-//        }
-//
-//    }
+
 
 }
